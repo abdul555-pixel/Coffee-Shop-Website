e6d040fbaf5b5cd4bc38ed38246577c58d820644
Abdullah <abdullahmasood068@gmail.com>
2025-01-03 16:31:43 +0500

    slider-img transition

---
+++ script.js
@@ -0,0 +1,38 @@
+const slides = document.querySelectorAll('.slide');
+let count = 0;
+
+slides.forEach(
+    (slide, index) => {
+        slide.style.left = `${index * 100}%`
+    }
+);
+
+const slideImage = () => {
+    slides.forEach(
+        (slide) => {
+            slide.style.transform = `translateX(-${count * 100}%)`;
+        }
+    )
+}
+
+const resetImage = () => {
+    slides.forEach(
+        (slide) => {
+            slide.style.transform = `translateX(${count * 100}%)`;
+        }
+    )
+}
+
+setInterval(() => {
+    if(count <= 0) {
+        count++;
+        slideImage();
+    }
+}, 5000);
+
+setInterval(() => {
+    if(count == (slides.length - 1)) {
+        count--;
+        resetImage();
+    }
+}, 10000)
